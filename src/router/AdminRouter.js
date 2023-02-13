f650ae75fed81876f01519befc3904c964c70af4
abishkar rai <raiabiskar0.5@gmail.com>
2023-02-13 13:30:36 +1100

    register page complete

--- src/router/AdminRouter.js
+++ src/router/AdminRouter.js
@@ -5,7 +5,7 @@ import { hashPassword } from '../utils/bcrypt.js';
 
 const router = express.Router();
 import { v4 as uuidv4 } from 'uuid';
-import { newAccountEmailVerificationEmail } from '../utils/nodemailer.js';
+import { emailVerifiedNotification, newAccountEmailVerificationEmail } from '../utils/nodemailer.js';
 
 
 
@@ -20,6 +20,42 @@ router.post("/", (req, res, next) => {
       next(error);
     }
   });
+
+  // admin user registration
+router.post("/register", newAdminValidation, async (req, res, next) => {
+  try {
+    req.body.password = hashPassword(req.body.password);
+    req.body.emailVerificationCode = uuidv4();
+
+    const result = await createNewAdmin(req.body);
+
+    if (result?._id) {
+      const uniqueLink = `${process.env.FRONTEND_ROOT_URL}/verify?c=${result.emailVerificationCode}&email=${result.email}`;
+
+      newAccountEmailVerificationEmail(uniqueLink, result);
+
+      res.json({
+        status: "success",
+        message:
+          "We have send a verification email. Please check your email, inclucing junk folder, and follow the instruction to verify your account.",
+      });
+
+      return;
+    }
+
+    res.json({
+      status: "error",
+      message: "Error, Unable to create a new user has been registered",
+    });
+  } catch (error) {
+    if (error.message.includes("E11000 duplicate key error collection")) {
+      error.message =
+        "There is already account exist associated with this email";
+      error.errorCode = 200;
+    }
+    next(error);
+  }
+});
   
  // admin user email verification
 router.post("/verify", emailVerificationValidation, async (req, res, next) => {
